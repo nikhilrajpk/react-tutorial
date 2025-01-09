e524e1a19577818e08d43f23dc61c275d039ee11
nikhilrajpk <nikhilrajpk2002@gmail.com>
2025-01-09 18:56:44 +0530

    01basics done

---
+++ react-tutorial/01basicreact/src/App.js
@@ -0,0 +1,13 @@
+
+import Youtube from './Youtube'
+
+function App() {
+  return (
+    <>
+      <h1>Classic React basic</h1>
+      < Youtube />
+    </>
+  );
+}
+
+export default App;
---
+++ react-tutorial/01basicvite/src/App.jsx
@@ -0,0 +1,12 @@
+import Youtube from "./Youtube"
+
+function App() {
+  return (
+    <>
+      <h1>Vite React app</h1>
+      < Youtube />
+    </>
+  )
+}
+
+export default App
